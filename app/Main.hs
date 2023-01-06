2aaee1bfca71310d6aaa4cefc1e9c3ddf2011bd2
wrvsrx <wrvsrx@outlook.com>
2023-01-06 17:24:24 +0800

    feat: initial commit

---
+++ app/Main.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Data.Text.Encoding.Base64 (encodeBase64)
+import Data.Text.IO as T
+
+main :: IO ()
+main = T.getContents >>= (\cnt -> T.putStrLn $ "\ESC]52;c;" <> encodeBase64 cnt <> "\BEL")
---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "hello world";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      rec {
+        packages.default = pkgs.callPackage ./default.nix { };
+        devShells.default = pkgs.mkShell { inputsFrom = [ packages.default.env ]; };
+        formatter = pkgs.nixpkgs-fmt;
+      }
+    );
+}
